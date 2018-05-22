0416a679fea493231cf0f1c2bede6cd11629dd77
Mario <msiu001@fiu.edu>
2018-05-22 14:54:00 -0400

    creating a custom route, bad bad way to do it, it is not recommendable to do it this way

--- Moviely/Moviely/Controllers/MoviesController.cs
+++ Moviely/Moviely/Controllers/MoviesController.cs
@@ -12,9 +12,31 @@ namespace Moviely.Controllers
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Sherk" };
+            var movie = new Movie() { Name = "Shrek" };
 
             return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex= {0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate( int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
