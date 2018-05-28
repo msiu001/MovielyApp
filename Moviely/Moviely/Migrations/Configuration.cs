b5d70550e502a4b15cac04bcedda818c24209e21
Mario <msiu001@fiu.edu>
2018-05-28 17:56:33 -0400

    enable-migration with the creation of the DBContext class

---
+++ Moviely/Moviely/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Moviely.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Moviely.Models.MyDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Moviely.Models.MyDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Moviely/Moviely/Models/MyDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Entity;
+
+namespace Moviely.Models
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext() { }
+    }
+}
